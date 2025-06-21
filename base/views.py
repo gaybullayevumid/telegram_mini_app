07ca0565591d5e94d30e870ac35bb4bef3ac01c1
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-06-21 11:23:04 +0500

    Add hello_world and api_hello view functions for basic responses

--- base/views.py
+++ base/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def hello_world(request):
+    return render(request, 'base.html')
+
+def api_hello(request):
+    return JsonResponse({'message': 'Hello World from Django!'})
